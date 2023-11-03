a979f7fd8d921be59f046f3ce41779621b895926
Vachagan <113465348+Metax7@users.noreply.github.com>
2023-11-03 12:25:18 +0400

    create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
